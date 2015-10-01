07b9f9622ef6ae08eec3f8032e48f5f4a981aecb
Davi Chaves <dchaves@usc.edu>
2015-10-01 16:29:09 -0700

    first commit

---
+++ DaviRodriguesChaves.cpp
@@ -0,0 +1,28 @@
+////    DO NOT EDIT BELOW   ////
+#include <iostream>
+#include <time.h>
+#include <string>
+
+using namespace std;
+
+struct IceCream{
+	int value;
+	int price;
+};
+
+int buyIceCream(int budget, IceCream ic[], int n);
+
+////    DO NOT EDIT ABOVE   ////
+
+// Note: If you need to you can put includes here
+
+const string STUDENT_NAME = "your name here";
+const string STUDENT_EMAIL = "your email here";
+
+int buyIceCream(int budget, IceCream ic[], int n) {
+    //Implement buyIceCream here
+}
+
+////    DO NOT EDIT BELOW   ////
+
+// Ian will paste test cases here
