adeb3a5e01528baa823167f5f493d56bd10cffe1
Davi Chaves <dchaves@usc.edu>
2015-10-08 13:51:00 -0700

    submission

--- DaviRodriguesChaves.cpp
+++ DaviRodriguesChaves.cpp
@@ -14,8 +14,6 @@ int buyIceCream(int budget, IceCream ic[], int n);
 
 ////    DO NOT EDIT ABOVE   ////
 
-using namespace std;
-
 const string STUDENT_NAME = "Davi Rodrigues Chaves";
 const string STUDENT_EMAIL = "dchaves@usc.edu";
 
@@ -30,36 +28,20 @@ int getMax(int a, int b) {
 int buyIceCream(int budget, IceCream ic[], int n) {
 	
 	int matrix[n+1][budget+1];
-
-	if (n == 0 || budget = 0) {
-		return 0;
-	}
  
-   for (int i = 0; i <= n; i++) { //building the matrix
-       for (int j = 0; j <= W; j++) {
-           	if (i==0 || j==0) {
-            	matrix[i][j] = 0; //first row and column are zero
-            } else if (ic[i-1].price <= j) {
-            	matrix[i][j] = getMax(val[i-1] + matrix[i-1][j-ic[i-1].price],  matrix[i-1][j]);
-           	} else {
-            	matrix[i][j] = matrix[i-1][j];
-           	}
-       }
-   }
+	for (int i = 0; i <= n; i++) { //building the matrix
+		for (int j = 0; j <= budget; j++) {
+			if (i==0 || j==0) {
+				matrix[i][j] = 0; //first row and column are zero
+			} else if (ic[i-1].price <= j) {
+				matrix[i][j] = getMax(ic[i-1].value + matrix[i-1][j-ic[i-1].price],  matrix[i-1][j]);
+			} else {
+				matrix[i][j] = matrix[i-1][j];
+			}
+		}
+	}
    // runtime of this is o(n*budget)
    return matrix[n][budget];
-}
-
-int main(int argc, char const *argv[]) {
-	cout << "hello!\n";
-	IceCream ic[80];
-	for (int i = 0; i < 80; i++){
-		ic[i].price = i;
-		ic[i].value = (i - 1)*5 + 3;
-	}
-	cout << "The maximum number of ice creams Ian can buy is:"; 
-	cout << buyIceCream(2000, ic, 80) << " \n";
-	return 0;
 }
 
 ////    DO NOT EDIT BELOW   ////
